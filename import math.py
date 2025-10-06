d2b5a0e81694471497552e4d18267534f5afe253
sina karimi <sinak5081@gmail.com>
2025-10-06 10:20:39 -0700

    import math

---
+++ import math.py
@@ -0,0 +1,7 @@
+import math
+r = float(input("Enter the radius:"))
+print("radius:",r)
+area = (math.pi * r**2)
+print("result of the area of the circle is:",area)
+primeter = (2 * math.pi * r)
+print("result of the primeter of the circle is:",primeter) 
