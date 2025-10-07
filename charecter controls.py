c240d57c9c2fcb0251da915d9788e040161107a4
sina karimi <sinak5081@gmail.com>
2025-10-07 09:42:45 -0700

    character controls

---
+++ charecter controls.py
@@ -0,0 +1,20 @@
+print("\n")
+#Create a new line
+print("\\")
+#print this \ charecter
+print("\'sina\' \'karimi\'")
+#Prints the character with quotation marks
+print("\"sina\" \"karimi\"")
+#Print the character with double qoutation marks
+print("\a")
+#it plays a beeping sound
+print("s\bsina")
+#Deletes the character before it
+print("\f")
+#Creates a new page
+print("\n")
+#creates a new line
+print("\N{HEAVY BLACK HEART}")
+# Print unicode character with specified name
+print("\000 HEAVY BLACK HEART")
+print("\uhhhh HEAVY BLACK HEART \uhhhh")
