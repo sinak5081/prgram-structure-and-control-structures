66fffa38269599a734e9c3cfd34303dec6c2322d
sina karimi <sinak5081@gmail.com>
2025-10-08 08:34:42 -0700

    Using the math founction we calculated the cube of three numbers added them up and diplayed them in the output

---
+++ Bignner project/Calculating the cube of a numbers.py
@@ -0,0 +1,6 @@
+import math
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+sum = (math.pow(num1 , 3) + math.pow(num2, 3) + math.pow(num3,3))
+print("sum of cube numbers is :{0:8.2f}".format(sum))
