90b242434f388325481615e409a7e71dfcd3fe6f
sina karimi <sinak5081@gmail.com>
2025-10-09 23:58:29 -0700

    We take the hours , minutes and seconds from the input and then convert them all to seconds and show them in the output

---
+++ Bignner project/convert hours to seconds.py
@@ -0,0 +1,5 @@
+hours = int(input("please enter hours: "))
+minutes = int(input("please Enter minutes: "))
+seconds = int(input("please Enter seconds: "))
+sum_of_seconds = (hours * 3600)+(minutes * 60)+seconds
+print("sum of seconds is : ",sum_of_seconds)
