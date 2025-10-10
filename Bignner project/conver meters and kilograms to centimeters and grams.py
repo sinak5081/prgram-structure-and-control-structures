9b3e61cebf7f53dbc380a527827a5158b8fa3a31
sina karimi <sinak5081@gmail.com>
2025-10-09 23:41:24 -0700

    We take height and weight in meters and kilometers from the input and convert them to centimeters and grams and show them in the output

---
+++ Bignner project/conver meters and kilograms to centimeters and grams.py
@@ -0,0 +1,6 @@
+height = float(input("Enter your height(meter): "))
+weight = float(input("Enter your weight(kilogram): "))
+Centimeter = height * 100
+gram = weight * 1000
+print("height in centimeters: ",Centimeter)
+print("weight in grams: ",gram)
