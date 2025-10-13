cbd2ae95c68b9b293a062bfb135a43be0b3e0303
sina karimi <sinak5081@gmail.com>
2025-10-13 04:56:17 -0700

    calculate two numbers using the input operator

---
+++ Control structures/Calculating the value of two numbers with the operator from the input.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter your first number: "))
+num2  = int(input("Enter your second number: "))
+operator = (input("enter your operator(+,-,*,/): "))
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator == "*":
+    print(num1*num2)
