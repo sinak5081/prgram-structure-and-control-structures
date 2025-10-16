cc15009fd3de1a9576e1694d2f48fbc115884ced
sina karimi <sinak5081@gmail.com>
2025-10-16 04:50:55 -0700

    You have completed an equation using the range function and the increment step in the for structure.

---
+++ Control structures/Range function and increment step.py
@@ -0,0 +1,8 @@
+x = int(input("Enter your number for x: "))
+n = int(input("Enter your number for N: "))
+sum = 0
+sign = 1
+for i in range(1,n + 1,2):
+    sum = sum + (x ** i) * sign
+    sign = -sign
+print("sum : ",sum)    
